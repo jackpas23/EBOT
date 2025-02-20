1f6bc72cfd068faf7d7abbd327d0a9268191999a
Flaxo P <jack.f.pas@gmail.com>
2025-02-20 17:07:41 -0500

    fixed deploy script, need to adjust manifest path

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,12 @@
 
 ##In firefox extensions add the manfiest, if snap requires different path
 
-if test -e "~/.mozilla/native-messaging-hosts"; then
+if test -e "$HOME/.mozilla/native-messaging-hosts"; then
   echo "Path exists"
 else
-  mkdir -p "~/.mozilla/native-messaging-hosts"
+  mkdir -p "$HOME/.mozilla/native-messaging-hosts"
 fi
 sed -i "s|\USER|$(whoami)|g" ./host/bbot_host.json
-
+cp ./host/bbot_host.json ~/.mozilla/native-messaging-hosts
+chmod +x ./host/bbot_host.py
+pipx install bbot
