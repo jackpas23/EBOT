d7defe05aac8dc8bdec96df228942802420cb7b8
dark <dark@federal>
2025-03-04 19:33:35 +0000

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,26 @@
-#!/bin/bash
-
 ##In firefox extensions add the manfiest, if snap requires different path
-if snap list | grep -q firefox; then
-   mkdir  "$HOME/snap/firefox/common/.mozilla/native-messaging-hosts"
-##need to flesh this out, just not concern now
-else
-   echo "firefox not installed with snap"
+
+
+
+
 if test -e "$HOME/.mozilla/native-messaging-hosts"; then
+
   echo "Path exists"
+
 else
+
+
   mkdir -p "$HOME/.mozilla/native-messaging-hosts"
+
 fi
+
 sed -i "s|\USER|$(whoami)|g" ./host/bbot_host.json
+
+
 cp ./host/bbot_host.json ~/.mozilla/native-messaging-hosts
+
+
 chmod +x ./host/bbot_host.py
+
+
 pipx install bbot
