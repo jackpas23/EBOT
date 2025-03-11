cd707db665e60216cb3b48cb73b410691c00cba7
Flaxo P <jack.f.pas@gmail.com>
2025-03-11 19:40:20 -0400

    added scrool to top btn

--- popup.js
+++ popup.js
@@ -34,6 +34,20 @@ document.addEventListener("DOMContentLoaded", async () => {
     browser.runtime.sendMessage({ type: "getOutput" });
     browser.runtime.sendMessage({ type: "getOutfile" });
     browser.runtime.sendMessage({ type: "getHosts" });
+    let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
     
     
     function updateDropdown(filePaths) {
@@ -137,6 +151,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     
     clearOutputBtn.addEventListener("click", () => {
         browser.runtime.sendMessage({ type: "clearOutput" });
+    });
+    myBtn.addEventListener("click", () => {
+        window.scrollTo({top:0, behavior: 'smooth'}) = 0;
     });
     extractStreamUrlsBtn.addEventListener("click", () => {
         browser.runtime.sendMessage({ type: "getURLS" });
